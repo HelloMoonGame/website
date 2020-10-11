2e0c3a738fb3c3cf8331c7bcfa5f232a26aa26f4
Vincent Bitter <vincent@logiqs.nl>
2020-10-11 17:33:33 +0200

    Make Travis build job work  (#3)
    Automatically test and build the website

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -1,20 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface FeatureProps {
-    name: String
-    description: String
-    icon
+  name: string
+  description: string
+  icon
 }
 
-const Feature = ({ name, description, icon }: FeatureProps) =>
-    <div className="col-md-4 text-center pb-5">
-        <div className="mb-3">
-            <div className="d-inline-block p-5 bg-white rounded-circle">
-                <FontAwesomeIcon icon={icon} size="3x" fixedWidth className="square-icon" />
-            </div>
-        </div>
-        <h4>{name}</h4>
-        <p>{description}</p>
+const Feature = ({ name, description, icon }: FeatureProps) => (
+  <div className="col-md-4 text-center pb-5">
+    <div className="mb-3">
+      <div className="d-inline-block p-5 bg-white rounded-circle">
+        <FontAwesomeIcon
+          icon={icon}
+          size="3x"
+          fixedWidth
+          className="square-icon"
+        />
+      </div>
     </div>
+    <h4>{name}</h4>
+    <p>{description}</p>
+  </div>
+)
 
 export default Feature
