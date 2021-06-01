07c15f14d60e50d9a9244ff1649c47e268af2938
Vincent Bitter <vincent@vincentbitter.nl>
2021-06-01 22:01:33 +0200

    Remove Bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import 'bootstrap/scss/bootstrap.scss'
 import '../styles/main.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
