15d23f9fe1d86e7f7a3acb33edafd05c55782400
Vincent Bitter <vincent@vincentbitter.nl>
2021-06-05 17:33:50 +0200

    Fix test

--- src/jest.config.js
+++ src/jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '^.+\\.(gif|ttf|eot|svg|png|jpg)(\\?.+)?$':
+      '<rootDir>/test/__mocks__/fileMock.js',
   },
 }
